f3df6da4a5025999e5b936c0b41c8b00ac41a639
Hopeworks N Camden <hopeworks@dev.(none)>
2014-10-31 09:17:18 -0400

    added pinterest style pages

---
+++ page-Categories.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name:Categories Page Template
+*/
+?>
+
+<?php get_header(); ?>
+		<div class="row">
+			<div class="large-12 columns">
+			page-Categories.php
+				<div id="masonryContainer">
+					<?php $args = array( 'post_type' => 'Pinterestss', 'posts_per_page' => 13 ); ?>
+					<?php $loop = new WP_Query( $args ); 
+		  				if ($loop->have_posts()) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			
+		    		<?php get_template_part(  'partials/content', 'masonry' ); ?>
+			
+		    		<?php endwhile; ?>	
+		    	</div>
+			</div>
+			<div class="medium-12 columns">
+		        <?php if (function_exists('joints_page_navi')) { ?>
+		        <?php joints_page_navi(); ?>
+		        <?php } else { ?>
+		      
+		        <nav class="wp-prev-next">
+			        <ul class="clearfix">
+						<li class="prev-link"><?php next_posts_link(__('&laquo; Older Entries', "jointstheme")) ?></li>
+					    <li class="next-link"><?php previous_posts_link(__('Newer Entries &raquo;', "jointstheme")) ?></li>
+					</ul>
+				</nav>
+				<?php } ?>		
+				
+				<?php else : ?>
+					    
+    				<?php get_template_part( 'partials/content', 'missing' ); ?>
+					
+				<?php endif; ?>
+			</div>
+			<div class="row" id="Pinterest" data-masonry-options='{ "itemSelector": ".masonry-entry" }'>
+		
+			
+			</div> 	
+		</div><!-- end #stories -->
+		
+
+<?php get_footer(); ?>
+
+
+
---
+++ partials/content-pinterest.php
@@ -0,0 +1,139 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+	
+<!--  THIS AREA IS FOR THE SEARCH FORM -->
+<form>
+ 	<fieldset>
+    <legend>Refine Search</legend>
+	<div class="row">
+		<div class="large-6 columns">
+	    	<label>Program Name:
+	        	<input type="text" placeholder="Enter Program Name" />
+	      	</label>
+	    </div>
+		<div class="large-3 columns">
+	      	<label>Program Type:
+	        	<select>
+	          		<option value="husker">Husker</option>
+	          		<option value="starbuck">Starbuck</option>
+	          		<option value="hotdog">Hot Dog</option>
+	          		<option value="apollo">Apollo</option>
+	        	</select>
+	    	</label>
+		</div>
+		<div class="large-3 columns">
+      		<label>Zip Code
+        		<input type="text" placeholder="Zip Code" />
+      		</label>
+    	</div>
+    	
+    	<div class="large-4 columns">
+      		<label>Language spoken at Program:</label>
+      		<input id="english" type="checkbox"><label for="english">English</label>
+      		<input id="spanish" type="checkbox"><label for="spanish">Spanish</label>
+      		<input id="otherlang" type="checkbox"><label for="otherlang">Other</label>
+    	</div>
+    	<div class="large-3 columns">
+	      	<label>Types of Services:
+	        	<select>
+	          		<option value="husker">Husker</option>
+	          		<option value="starbuck">Starbuck</option>
+	          		<option value="hotdog">Hot Dog</option>
+	          		<option value="apollo">Apollo</option>
+	        	</select>
+	    	</label>
+		</div>
+		<div class="large-3 columns">
+	      	<label>Age Group:
+	        	<select>
+	          		<option value="husker">Husker</option>
+	          		<option value="starbuck">Starbuck</option>
+	          		<option value="hotdog">Hot Dog</option>
+	          		<option value="apollo">Apollo</option>
+	        	</select>
+	    	</label>
+		</div>
+		<div class="large-2 columns">
+			<a href="#" class="button radius right expand success">Search</a>
+		</div>
+    	
+	</div>
+ 
+	</fieldset>
+</form>
+
+
+<!-- END SEARCH FORM -->
+<p></p>
+<header class="article-header">	
+		<h2 class="entry-title single-title" itemprop="headline">Category Name</h2>
+		<?php //get_template_part( 'partials/content', 'byline' ); ?>
+    </header> <!-- end article header -->
+<p></p>
+<!--  Insert Google Map Here -->
+<div class="row">
+	<div class="large-8 large-offset-2 end columns">
+	<!-- <img src="/wp-content/uploads/2014/10/sf-search-results.png" /> -->
+	</div>
+</div>
+THIS IS A DETAILS PAGE
+<!--  END GOOGLE MAP -->
+<div class="row">
+<div class="large-6 large-offset-1 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+<div class="large-5 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+<div class="large-6 large-offset-1 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+<div class="large-5 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+<div class="large-6 large-offset-1 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+<div class="large-5 columns">
+<h4>Organization Name</h4>
+<h5 class="middleblue">Program Name</h5>
+<p>Primary Contact Name<br/>
+Address<br/>
+<img src="/wp-content/uploads/2014/10/lightbluephone.png" class="phone" /> 444.333.2323<br/>
+<img src="/wp-content/uploads/2014/10/blueweb.png" class="globe" /><a href="#">www.Website.com</a></p>
+</div>
+
+
+
+</div>
+
+
